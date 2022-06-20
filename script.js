e6b9099aaf265c9f506e0582861586fe4feb0e8d
uknown-me <64226929+uknown-me@users.noreply.github.com>
2022-06-21 00:31:20 +0400

    add index.html add script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+let userName = "zuka";
+// console.log(userName)
+let userSurname = "khimshiasvili";
+let userAge = 20;
+let userEmail = "zukakhimshiashvili1@gmail.com";
+let userCity = "gori";
+
+
+
+let myName = "zuka";
+let myHobby = "football";
+let yearOfBirth = 2001;
+let currentYear = 2022;
+
+
+
+let textOne = "My name is " + myName + " " + " i am " + (currentYear - yearOfBirth) + " " +  "old and my hobby is " + myHobby;
+
+// console.log(textOne)
+
+let textTwo = `My name is ${myName} i am ${currentYear - yearOfBirth} old and my hobby is ${myHobby}`;
+
+console.log(textTwo)
